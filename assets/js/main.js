8db34431e34643b991fd0b29a9f741bfe57df8bd
Nadal Soler <nadalsoler@gmail.com>
2022-04-23 01:37:19 +0200

    setup: Hugo scaffolding

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// The document ready event executes already when the HTML-Document is loaded //
+// and the DOM is ready, even if all the graphics haven't loaded yet.         //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("This is JS babe!");
+});
---
+++ content/pattern-library.md
@@ -0,0 +1,8 @@
+---
+title: "Pattern Library"
+date: 2022-04-23T01:17:36+02:00
+draft: false
+type: "page"
+layout: "pattern-library"
+sitemap_exclude: true
+---
